494cdfa877da1990748ab08485f0db90dd4f8e93
yu-ya <talesoftoho@gmail.com>
2026-03-21 21:12:17 +0900

    メモ新規作成機能と一覧表示を実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,5 +3,27 @@
 class MemosController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @memos = current_user.memos.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @memo = current_user.memos.new
+  end
+
+  def create
+    @memo = current_user.memos.new(memo_params)
+
+    if @memo.save
+      redirect_to memos_path, notice: 'メモを作成しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:title, :content)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,12 +6,12 @@ require 'rails/test_help'
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
+  end
+end
 
-    # Add more helper methods to be used by all tests here...
+module ActionDispatch
+  class IntegrationTest
+    include Devise::Test::IntegrationHelpers
   end
 end
