8fcbb52db5a635196fb7ba60ece062c495e5aeeb
yu-ya <talesoftoho@gmail.com>
2026-03-27 20:50:54 +0900

    add: GeneratedTextモデルの追加

---
+++ app/models/generated_text.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class GeneratedText < ApplicationRecord
+  belongs_to :memo
+
+  enum :kind, { organize: 0, summarize: 1, writing: 2 }
+
+  validates :content, presence: true
+  validates :kind, presence: true
+end
