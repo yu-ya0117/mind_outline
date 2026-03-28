02a40ce2c800d5175f255b142036a3a254724b08
yu-ya <talesoftoho@gmail.com>
2026-03-28 22:23:07 +0900

    fix: AI整理のDB保存処理を修正

--- app/controllers/generated_texts_controller.rb
+++ app/controllers/generated_texts_controller.rb
@@ -1,14 +1,22 @@
+# frozen_string_literal: true
+
 class GeneratedTextsController < ApplicationController
   before_action :authenticate_user!
 
   def create
     @memo = Memo.find(params[:memo_id])
-    GeneratedText.create!(
-      memo: @memo,
-      kind: params[:kind],
-      content: params[:content]
-    )
+    generated_text = @memo.generated_texts.build(generated_text_params)
 
-    redirect_to memo_path(@memo), notice: "保存しました"
+    if generated_text.save
+      redirect_to memo_path(@memo), notice: '生成結果を保存しました'
+    else
+      redirect_to memo_path(@memo), alert: '生成結果の保存に失敗しました'
+    end
+  end
+
+  private
+
+  def generated_text_params
+    params.require(:generated_text).permit(:kind, :content)
   end
 end
---
+++ test/controllers/generated_texts_controller_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class GeneratedTextsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+    @memo = memos(:one)
+  end
+
+  test '生成結果を保存できる' do
+    assert_difference('GeneratedText.count', 1) do
+      post memo_generated_texts_path(@memo), params: {
+        generated_text: {
+          kind: 'organize',
+          content: "- AI処理\n  - 内容: テスト"
+        }
+      }
+    end
+
+    generated_text = GeneratedText.last
+    assert_equal @memo.id, generated_text.memo_id
+    assert_equal 'organize', generated_text.kind
+    assert_equal "- AI処理\n  - 内容: テスト", generated_text.content
+    assert_response :redirect
+  end
+
+  test 'contentが空だと保存できない' do
+    assert_no_difference('GeneratedText.count') do
+      post memo_generated_texts_path(@memo), params: {
+        generated_text: {
+          kind: 'organize',
+          content: ''
+        }
+      }
+    end
+
+    assert_response :redirect
+  end
+end
