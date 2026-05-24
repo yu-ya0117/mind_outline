479686693bf43823d957674a84a38a698d0c02d3
yu-ya <talesoftoho@gmail.com>
2026-05-24 18:21:45 +0900

    add: パスワードリセット機能実装のためマイグレーションをusersテーブルに適用

---
+++ db/migrate/20260524090802_add_recoverable_to_users.rb
@@ -0,0 +1,8 @@
+class AddRecoverableToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
+
+    add_index :users, :reset_password_token, unique: true
+  end
+end
